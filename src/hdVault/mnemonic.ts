eed1497a7f1e97a47a7d24956e229388a0d986e7
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-04-12 20:46:13 -0400

    QB-401: Implement mnemonic utils

--- src/hdVault/mnemonic.ts
+++ src/hdVault/mnemonic.ts
@@ -1,7 +1,41 @@
-export const generatePhrase = (phraseLength: number): string[] => {
-  return [''];
+import { entropyToMnemonic, generateMnemonic, validateMnemonic, wordlists } from 'bip39';
+import crypto from 'crypto';
+
+const mnemonic12 = 12;
+const mnemonic24 = 24;
+
+interface MnemonicItem {
+  readonly word: string;
+  readonly index: number;
+}
+
+type MnemonicPhrase = MnemonicItem[];
+
+type MnemonicLength = typeof mnemonic12 | typeof mnemonic24;
+
+export const convertArrayToString = (mnemonicArray: MnemonicPhrase): string => {
+  return mnemonicArray.map(({ word = '' }) => word).join(' ');
 };
 
-export const verifyPhrase = (phrase: string[]): boolean => {
-  return true;
+export const convertStringToArray = (mnemonicStr: string): MnemonicPhrase => {
+  const mnemonicArray = mnemonicStr.split(' ');
+  return mnemonicArray.map((word: string, idx: number): MnemonicItem => ({ index: idx + 1, word }));
+};
+
+export const generateMnemonicPhrase = (phraseLength: MnemonicLength): MnemonicPhrase => {
+  let mnemonicString = '';
+
+  if (phraseLength === mnemonic12) {
+    mnemonicString = generateMnemonic();
+    return convertStringToArray(mnemonicString);
+  }
+
+  const entropy = crypto.randomBytes(32);
+  mnemonicString = entropyToMnemonic(entropy);
+
+  return convertStringToArray(mnemonicString);
+};
+
+export const verifyPhrase = (phrase: MnemonicPhrase): boolean => {
+  return validateMnemonic(convertArrayToString(phrase), wordlists.english);
 };
