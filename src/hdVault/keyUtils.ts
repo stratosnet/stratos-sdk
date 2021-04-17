c5f95ef8825bc9fd5f85d10596f72ced38438057
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-04-16 21:27:31 -0400

    QB-402: Refactor key utils

--- src/hdVault/keyUtils.ts
+++ src/hdVault/keyUtils.ts
@@ -1,34 +1,24 @@
-import * as bech32 from 'bech32';
 import { mnemonicToSeedSync } from 'bip39';
 import sjcl from 'sjcl';
 import nacl from 'tweetnacl';
 import naclUtil from 'tweetnacl-util';
 
 import { convertArrayToString, MnemonicPhrase } from './mnemonic';
-import { bufferToUint8Array, hexStrToUint8Array, uint8ArrayToBase64str, uint8ArrayToBuffer } from './utils';
+import { bufferToUint8Array, uint8ArrayToBase64str, uint8ArrayToBuffer } from './utils';
 
-// @todo move to the config - to be created
-const masterSeedKeyAddrPrefix = 'st';
+export interface KeyPair {
+  publicKey: string;
+  privateKey: string;
+}
 
 export const generateMasterKeySeed = (phrase: MnemonicPhrase): Buffer => {
   return mnemonicToSeedSync(convertArrayToString(phrase));
 };
 
 export const getMasterKeySeedPublicKey = (masterKeySeed: Buffer): string => {
-  return uint8ArrayToBase64str(nacl.sign.keyPair.fromSecretKey(masterKeySeed).publicKey);
-};
+  const publicKey = uint8ArrayToBase64str(nacl.sign.keyPair.fromSecretKey(masterKeySeed).publicKey);
 
-export const getMasterKeySeedAddress = (masterKeySeedPublicKey: string): string => {
-  const base64PublicKey = sjcl.codec.base64.toBits(masterKeySeedPublicKey);
-  const hash = new sjcl.hash.sha256();
-  hash.update(base64PublicKey);
-  const hashResult = hash.finalize();
-  const hashedData = sjcl.codec.hex.fromBits(hashResult);
-  hash.reset();
-  const data = hexStrToUint8Array(hashedData.substring(0, 40));
-  const address = bech32.encode(masterSeedKeyAddrPrefix, bech32.toWords(data));
-
-  return address;
+  return publicKey;
 };
 
 export const encryptMasterKeySeed = (password: string, masterKeySeed: Buffer): sjcl.SjclCipherEncrypted => {
@@ -50,14 +40,14 @@ export const encryptMasterKeySeed = (password: string, masterKeySeed: Buffer): s
 export const decryptMasterKeySeed = async (
   password: string,
   encryptedMasterKeySeed: string,
-): Promise<Buffer> => {
+): Promise<Buffer | false> => {
   let decryptedMasterKeySeed;
 
   try {
     const decrypteCypherText = sjcl.decrypt(password, encryptedMasterKeySeed);
     decryptedMasterKeySeed = uint8ArrayToBuffer(naclUtil.decodeBase64(decrypteCypherText));
   } catch (err) {
-    return Promise.reject(Buffer.from(''));
+    return Promise.reject(false);
   }
 
   if (decryptedMasterKeySeed) {
@@ -77,9 +67,4 @@ export const unlockMasterKeySeed = async (
   } catch (e) {
     return Promise.resolve(false);
   }
-};
-
-export const getMasterKeySeedAddressFromPhrase = (phrase: MnemonicPhrase): string => {
-  const masterKeySeed = generateMasterKeySeed(phrase);
-  return getMasterKeySeedAddress(getMasterKeySeedPublicKey(masterKeySeed));
 };
