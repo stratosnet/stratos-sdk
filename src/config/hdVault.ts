557c9efd223eade4b71e614b7b58afdf6c842204
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-04-16 21:27:59 -0400

    QB-402: Add hd vault config

---
+++ src/config/hdVault.ts
@@ -0,0 +1,8 @@
+export const masterkey = 'm/';
+export const bip44purpose = "44'/";
+export const keyPathSuffix = "'";
+
+export const stratosCoinType = "403'/0'/0'/";
+export const stratosAddressPrefix = 'st';
+
+export const keyPath = `${masterkey}${bip44purpose}${stratosCoinType}`;
