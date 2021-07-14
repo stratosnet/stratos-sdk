29f042f74774892ce3452e22eb704fd1c74ababd
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-07-13 22:21:17 -0400

    feat: minor bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+export * as accounts from './accounts';
+export * as accountTypes from './accounts/types';
 export * as hdVault from './hdVault';
 export { Random } from './random';
-export * as accounts from './transactions';
 export * as transactions from './transactions';
 export * as transactionTypes from './transactions/types';
-export * as accountTypes from './accounts/types';
 export * as validators from './validators';
 export * as validatorTypes from './validators/types';
