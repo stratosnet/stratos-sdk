8c9e87c615b0abd388907c22a510b02386719e76
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-08-13 09:11:30 -0400

    feat: qb-557: update max gas amount value

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -1,4 +1,4 @@
 export const decimalPrecision = 9;
 export const decimalShortPrecision = 4;
 export const standardFeeAmount = 50000; // in ustos
-export const maxGasAmount = 400000; // in ustos so it gives price of 0.125
+export const maxGasAmount = 200000; // in ustos so it gives gas price of 50_000 / 200_000
