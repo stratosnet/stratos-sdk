f6d1825de187446bee05ba05e3def2b7886e5ff0
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2024-07-18 17:29:49 -0400

    feat: st-359: version up

--- src/run.ts
+++ src/run.ts
@@ -429,6 +429,16 @@ async function testRedis() {
     },
 
     {
+      id: 3,
+      foo: {
+        bar: '2barfoo then and now',
+        foobar: false,
+      },
+      children: null,
+      anotherthing: 'cool',
+    },
+
+    {
       id: 2,
       foo: {
         bar: 'barfoo then and now',
@@ -439,10 +449,10 @@ async function testRedis() {
     },
   ];
 
-  // const sampleData = data;
+  const sampleData = data;
 
-  // const setRes = await FileDrive.sendDataToRedis(derivedKeyPair, sampleData);
-  // console.log('setRes', setRes);
+  const setRes = await FileDrive.sendDataToRedis(derivedKeyPair, sampleData);
+  console.log('setRes', setRes);
 
   const decodedOriginal = await FileDrive.getDataFromRedis(derivedKeyPair);
   console.log('decoded user data from redis', JSON.stringify(decodedOriginal));
